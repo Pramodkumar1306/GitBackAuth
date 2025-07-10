30d13c5797b35fc2c5c71906f786874a0213c408
Pramodkumar1306 <pramod73kumar7415@gmail.com>
2025-07-10 19:20:31 +0530

    added

--- Server.js
+++ Server.js
@@ -13,8 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin:allowedOrigin , credentials: true}));
 
-const startServer = async () => {
-    await connectDB();
+connectDB();
     //API End Points
 app.get('/',(req,res) => {
     res.send("Api Is Working")
@@ -28,6 +27,4 @@ app.listen(PORT,()=> {
     console.log(`${PORT} Running`)
 })
 
-}
-
-startServer();
+ 
